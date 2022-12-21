5f4a3b352924d4e510f62957f2f6bf06000c1a56
Баярсайхан Тугулдур <114753949+tmpabsolute@users.noreply.github.com>
2022-12-21 15:06:14 +0300

    Update LAB5.py

--- LAB5.py
+++ LAB5.py
@@ -1,63 +1,48 @@
 
-class Rocket():
-	def __init__(self, mass, fuel, engine):
-		self.mass = mass
-		self.fuel = fuel
-		engine = engine
-		
-		
-class Vostok(Rocket):
-	def __init__(self):
-		super().__init__(6750, 2000, True)
-		#Проверяет, есть ли топливо в изначально заданных параметрах
-		if self.fuel == 0:
-			self.engine = False
-		else:
-			self.engine = True
-			#Метод, позволяющий израсходовать count кг топлива
-	def spend_fuel(self, count):
-		self.fuel = (int(self.fuel) - int(count))
-		self.mass = (int(self.mass) - int(count))
-		if (int(self.fuel) - int(count)) <= 0:
-			self.engine = False
-		if self.fuel > 0:
-			return True
-		else:
-			return False
-	def get_fuel_level(self):
-		return self.fuel
-	def get_total_weight(self):
-		return(self.mass)
-	def get_is_engine_running(self):
-		return(self.engine)
-		
-class main(Rocket):
-	def __init__(self):
-		super().__init__(int(input()), int(input()), True)
-		if self.fuel == 0:
-			self.engine = False
-		else:
-			self.engine = True
-		step = int(input())#Количество топлива, тратящегося каждую секунду
-		while self.engine == True:#Пока двигатель работает, топливо тратиться
-			self.fuel = self.fuel - step
-			self.mass = self.mass - step
-			if self.fuel - step < 0:
-				self.engine = False#Двигатель останавливается, когда топливо кончается
-			print(self.mass, self.fuel, self.engine)#Каждый этап выводятся параметры ракеты
-			
-			
-			
-	
 
-M = main()
-print(M.mass,'\n')
+	class Rocket:
+    
+    def __init__(self, mass, fuel, bool):
+        self.mass = mass
+        self.fuel = fuel
+        self.bool = bool
+        self.mass = mass + fuel 
+    #Расход топлива и определение если оно осталось после расхода
+    def spend_fuel(self, count): 
+        
+        if (self.fuel - count) >= 0:    
+            self.mass = self.mass - count
+            self.fuel = self.fuel - count
+            return True
+        #Если топливо недостаточно, вернётся  false
+        else:
+            if self.fuel < count:
+                self.fuel = 0 
+            self.bool = False
+            self.mass = self.mass - self.fuel
+            return False
+       
+  #Количество топлива в двигателе
+            
+    def get_fuel_level(self):
+        return self.fuel
+        
+        
+ #Определение общую массу Двигателя
+ 
+    def get_total_weight(self):
+        return self.mass
+    
+#Работает ли двигатель
+    
+    def get_is_engine_running(self):
+        return self.bool
+        
+        
+        
 
-			
-		
-R = Vostok()
+test_ = Rocket(700, 400, True)
 
-print(R.spend_fuel(2000))
-print(R.get_fuel_level())
-print(R.get_total_weight())
-print(R.get_is_engine_running())
+while test_.bool == True:
+    test_.spend_fuel(30)
+    print(test_.get_total_weight(), test_.get_fuel_level(), test_.bool)
